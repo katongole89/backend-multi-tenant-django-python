cdd369dd01315044323d5ac5f731dc9192841459
katongole89 <katongolerichard089@gmail.com>
2022-08-12 16:04:19 +0300

    added mapped the simpleTenancy view to url

--- simpleTenancy/urls.py
+++ simpleTenancy/urls.py
@@ -3,6 +3,6 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    re_path(r'^simple-data/?$', views.GroupsDataApiView.as_view()),
+    re_path(r'^simple-data/?$', views.SimpleTenancyApiView.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
