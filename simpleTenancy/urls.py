e447ef57e802643666ddb7e0be371e2bc700620d
katongole89 <katongolerichard089@gmail.com>
2022-08-12 16:08:11 +0300

    corrected url

--- simpleTenancy/urls.py
+++ simpleTenancy/urls.py
@@ -4,5 +4,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     re_path(r'^simple-data/?$', views.SimpleTenancyApiView.as_view()),
+    path('simple-data/<slug:company_url>/', views.SimpleTenancyApiView.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
