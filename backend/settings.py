f48b4e31f98d55d4441916fefe653d79b1d8b92b
katongole89 <katongolerichard089@gmail.com>
2022-08-12 13:22:35 +0300

    added third party apps in settiongs

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third party app
+    'corsheaders',
+    'rest_framework',
+
     #custom
     'simpleTenancy'
 ]
